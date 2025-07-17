a2b628ff27a0b66e2757b79dfb440f961d61de56
grz3chotnik <krzysztofkubinski2004@gmail.com>
2025-07-17 13:41:13 -0300

    Reservations page better responsiveness and made the form structure better + added labels

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,64 +1,62 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+}
+
+
 export default function Reservations() {
     return <div>
         <Nav/>
 
 
-
-
         <div className={"reservecontainer"}>
             <h1>Reserve a table</h1>
-            <div className={"reservelayout"}>
+            <form className={"reservelayout"}>
                 <div className={"row1"}>
-                    <select>
-                        <option>Date</option>
-                        <option>dddddd</option>
-                        <option>ddddd</option>
-                        <option>ddddd</option>
-                        <option>ddddd</option>
-                        <option>ddddd</option>
-                    </select>
-                    <select>
-                        <option>No. of People</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
+                    <label htmlFor={"date"}>Date</label>
+                    <input id={"date"} type={"date"}/>
 
-                    </select>
-                    <input placeholder={"First name"}/>
-                    <input placeholder={"E-mail"}/>
+                    <label htmlFor={"nopeople"}>Number of people</label>
+                    <input id={"nopeople"} type={"number"} placeholder={"Number of people"}/>
+
+                    <label htmlFor={"firstname"}>First name</label>
+                    <input id={"firstname"} type={"text"} placeholder={"First name"}/>
+
+                    <label htmlFor={"email"}>E-mail</label>
+                    <input id={"email"} type={"email"} placeholder={"E-mail"}/>
 
                 </div>
 
                 <div className={"row2"}>
-                    <select>
-                        <option>Time</option>
-                        <option>ddddd</option>
-                        <option>ddddd</option>
-                        <option>ddddd</option>
-                        <option>ddddd</option>
+                    {/*<form></form>*/}
 
-                    </select>
-                    <select>
+                    <label htmlFor={"time"}>time</label>
+                    <input id={"time"} type={"time"} placeholder={"Time"}/>
+
+                    <label htmlFor={"occasion"}>Occasion</label>
+                    <select id={"occasion"}>
                         <option>Occasion</option>
                         <option>Birthday</option>
                         <option>Business</option>
                         <option>Other</option>
                     </select>
-                    <input placeholder={"Last name"}/>
-                    <input placeholder={"Phone number"}/>
+
+                    <label htmlFor={"lastname"}>Last name</label>
+                    <input id={"lastname"} type={"text"} placeholder={"Last name"}/>
+
+                    <label htmlFor={"phonenumber"}>Phone number</label>
+                    <input id={"phonenumber"} type={"number"} placeholder={"Phone number"}/>
                 </div>
 
                 <div className={"row3"}>
                     <img src={"./restaurant.jpg"} alt={"seats"} height={"230px"}/>
                 </div>
-                <button className={"yellowbutton"}>Reserve a table</button>
+                <button type={"submit"} className={"yellowbutton"} onSubmit={handleSubmit}>Reserve a table</button>
 
-            </div>
+            </form>
 
         </div>
         {/*<div>*/}
